d702ec0c97ebf09c2c7a3d5f3a67c88bd7f722de
Tibi K <krisboi.tibi@gmail.com>
2020-12-09 15:31:29 +0100

    update run commands

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker run -tid \
     --name rpc \
     -p 3000:3000 \
     -v $PWD/config.json:/etc/config.json \
-    docker.pkg.github.com/moonlet/zilliqa-rpc-gateway/rpc-gateway:v0.1.0 \
+    docker.pkg.github.com/moonlet/zilliqa-rpc-gateway/rpc-gateway:v0.2.0 \
         node /app/index.js /etc/config.json
 
 docker logs rpc -f
