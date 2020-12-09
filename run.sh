d00559eb2bba3f5fd608bc649a5f596a4b2971a0
Tibi K <krisboi.tibi@gmail.com>
2020-12-09 15:24:43 +0100

    change listen address

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ docker rm rpc
 
 docker run -tid \
     --name rpc \
+    -p 3000:3000 \
     -v $PWD/config.json:/etc/config.json \
     docker.pkg.github.com/moonlet/zilliqa-rpc-gateway/rpc-gateway:v0.1.0 \
         node /app/index.js /etc/config.json
